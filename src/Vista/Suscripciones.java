2796dfee9904b5c9ecb75cd9d30b947ba56b3a1d
2emm4 <emmachumillaslopez@gmail.com>
2026-04-23 10:58:06 +0200

    cocaina

--- src/Vista/Suscripciones.java
+++ src/Vista/Suscripciones.java
@@ -176,10 +176,7 @@ public class Suscripciones extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null}
+
             },
             new String [] {
                 "ID", "Cliente", "Plan", "Precio", "Fecha Inicio", "Fecha Fin", "Estado"
